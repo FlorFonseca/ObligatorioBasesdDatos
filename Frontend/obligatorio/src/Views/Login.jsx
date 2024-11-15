a6d84a9bdf378b239e846f78d95d53b6b2523f26
FlorFonseca <florencia.fonseca@correo.ucu.edu.uy>
2024-11-15 09:04:38 -0300

    Arreglo del login

--- Frontend/obligatorio/src/Views/Login.jsx
+++ Frontend/obligatorio/src/Views/Login.jsx
@@ -6,15 +6,23 @@ export default function Login() {
     const [correo, setCorreo] = useState(''); 
     const [password, setPassword] = useState('');
 
-    const handlePrueba = async () => {
-      const response = await fetch("http://localhost:5000/login", {
-        method: "POST",
-        body: JSON.stringify({ correo, password }),
-      });
-      const data = await response.json();
+    const handlePrueba = async (e) => {
+      e.preventDefault();
+      try{
+          const response = await fetch("http://localhost:5000/login", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ correo, password }),
+          });
       
-      if(response.ok){
-        console.log("logueado");
+        if(response.ok){
+          const data = await response.json();
+          console.log(data);
+        }
+      }catch(error){
+        console.log("error", e);
       }
     };
 
@@ -22,7 +30,7 @@ export default function Login() {
       <div className="container">
         
         <form className="form" onSubmit={handlePrueba}>
-          <div className='titulo'>Bienvenid@</div>
+          <div className="titulo">Bienvenid@</div>
           <div className="input1">
             <input
               className="input"
